55f26fd03e770d8c8a5ef85e570bc4e75863eec8
beto franca <beto2franca@gmail.com>
2023-10-24 10:33:54 -0300

    ajustando menu 02

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function ClickMenu(){
     if(menu.style.display == 'block'){
         menu.style.display = 'none'
-    }else{
+    } else{
         menu.style.display = 'block'
+    }
+   
+}
+
+function MudouT(){
+    if(window.innerWidth >= 790){
+        menu.style.display = 'none'
     }
 }
